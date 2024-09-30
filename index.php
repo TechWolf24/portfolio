92cd24ec2837d90b3f3ab7aaab32c52f41fa64b8
TechWolf24 <amelomanovic-developer@gmail.com>
2024-09-30 22:04:09 +0200

    gotov nav i header

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Portfolio</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <!-- navigacija -->
+    <nav>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Works</a></li>
+        <li><a href="#">Blog</a></li>
+      </ul>
+    </nav>
+    <!-- Header -->
+    <header class="container">
+      <div>
+        <h1>
+          Hi, I am John, <br />
+          Creative Technologist
+        </h1>
+        <p>
+          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+          sint. Velit officia consequat duis enim velit mollit. Exercitation
+          veniam consequat sunt nostrud amet.
+        </p>
+        <a href="#" class="btn">Download Resume</a>
+      </div>
+      <div>
+        <img src="img/header.jpg" alt="profilna slika" />
+      </div>
+    </header>
+  </body>
+</html>
